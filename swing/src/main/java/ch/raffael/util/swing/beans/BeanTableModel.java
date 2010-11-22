37cce87f2b9f8f0791f79104cdd735bf73b4f9fd
Raffael Herzog <herzog@raffael.ch>
2010-11-22 18:51:05 +0100

    Fix getColumn().

--- swing/src/main/java/ch/raffael/util/swing/beans/BeanTableModel.java
+++ swing/src/main/java/ch/raffael/util/swing/beans/BeanTableModel.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -190,7 +190,7 @@ public class BeanTableModel<T> extends AbstractTableModel {
         if ( index <= 0 ) {
             throw new IllegalArgumentException("No column for property '" + propertyName + "'");
         }
-        return table.getColumn(index);
+        return table.getColumnModel().getColumn(index);
     }
 
     private PropertyDescriptor getDescriptor(@NotNull String propertyName) {
